960fa5ba3fd15158c932e4c86489e57ca0fd6686
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-04-06 16:14:24 +0200

    File tokenized

--- src/main.rs
+++ src/main.rs
@@ -1,217 +1,108 @@
-use std::f64::consts::PI;
-const MASS: f64 = 510998.9499961642f64;
-const C: f64 = 299792458f64;
+use std::fs::read_to_string;
 
-pub trait Tracking {
-    fn track(&self, beam: Beam) -> Beam;
+#[derive(Debug)]
+enum TokenType {
+    Word,
+    Value,
+    Ocurly,
+    Ccurly,
+    Colon,
 }
 
-struct AccCav {
-    length: f64,
-    voltage: f64,
-    freq: f64,
-    phi: f64,
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    value: String,
 }
 
-impl AccCav {
-    fn new(l: f64, v: f64, freq: f64, phi: f64) -> AccCav {
-        AccCav {
-            length: l,
-            voltage: v,
-            freq: freq,
-            phi: phi,
+fn parse_word(input: &mut String) -> Token {
+    let mut name: String = chop_character(input).to_string();
+    while !input.is_empty() {
+        if input.starts_with(|c: char| c == '_' || c.is_ascii_alphanumeric()) {
+            name.push(chop_character(input));
+        } else {
+            break;
         }
+    }
+    Token {
+        token_type: TokenType::Word,
+        value: name,
     }
 }
 
-struct Drift {
-    length: f64,
-    gamma0: f64,
-}
-
-impl Drift {
-    fn new(l: f64, g: f64) -> Drift {
-        Drift {
-            length: l,
-            gamma0: g,
+fn parse_digit(input: &mut String) -> Token {
+    let mut already_decimal: bool = false;
+    let mut already_exp: bool = false;
+    let mut name: String = chop_character(input).to_string();
+    while !input.is_empty() {
+        if input.starts_with(|c: char| c.is_ascii_digit()) {
+            name.push(chop_character(input));
+        } else if input.starts_with('e') {
+            if already_exp {
+                panic!("Attempt to add 'e' to a digit twice");
+            }
+            already_exp = true;
+            name.push(chop_character(input));
+        } else if input.starts_with('.') {
+            if already_decimal {
+                panic!("Attempt to add a second decimal point to a digit");
+            }
+            already_decimal = true;
+            name.push(chop_character(input));
+        } else {
+            break;
         }
+    }
+    Token {
+        token_type: TokenType::Value,
+        value: name,
     }
 }
 
-struct Dipole {
-    b_field: f64,
-    theta: f64,
-    gamma0: f64,
+fn chop_character(input: &mut String) -> char {
+    input.remove(0)
 }
 
-impl Dipole {
-    fn new(b: f64, angle: f64, g: f64) -> Dipole {
-        Dipole {
-            b_field: b,
-            theta: angle,
-            gamma0: g,
-        }
-    }
-}
-
-impl Tracking for Drift {
-    fn track(&self, beam: Beam) -> Beam {
-        let mut output_beam: Beam = vec![];
-        for electron in beam {
-            let t = electron.t;
-            let l = self.length;
-
-            let g0 = self.gamma0;
-            let g = electron.ke / MASS;
-
-            let beta = (1.0 - (1.0 / g.powi(2))).sqrt();
-            let beta0 = (1.0 - (1.0 / g0.powi(2))).sqrt();
-
-            let new_t = t + (l / C) * (1.0 / beta - 1.0 / beta0);
-
-            output_beam.push(Electron {
-                t: new_t,
-                ke: electron.ke,
+fn tokenize_file_contents(contents: &mut String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = vec![];
+    while !contents.is_empty() {
+        if contents.starts_with(|c: char| c.is_whitespace()) {
+            chop_character(contents);
+            continue;
+        } else if contents.starts_with(|c: char| c.is_ascii_alphabetic()) {
+            tokens.push(parse_word(contents));
+        } else if contents.starts_with(|c: char| c.is_ascii_digit()) {
+            tokens.push(parse_digit(contents));
+        } else if contents.starts_with('{') {
+            chop_character(contents);
+            tokens.push(Token {
+                token_type: TokenType::Ocurly,
+                value: "{".to_string(),
             });
-        }
-        output_beam
-    }
-}
-
-impl Tracking for Dipole {
-    fn track(&self, beam: Beam) -> Beam {
-        let mut output_beam: Beam = vec![];
-        for electron in beam {
-            let g0 = self.gamma0;
-            let g = electron.ke / MASS;
-
-            let pc0 = (g0.powi(2) - 1.0).sqrt() * MASS;
-            let pc = (g.powi(2) - 1.0).sqrt() * MASS;
-
-            let rho0 = pc0 / (C * self.b_field);
-            let rho = pc / (C * self.b_field);
-
-            let l0 = rho0 * self.theta;
-            let l = rho * self.theta;
-
-            let delta_l = l - l0;
-            let v = C * (1.0 - (1.0 / g.powi(2))).sqrt();
-
-            let new_t = electron.t + delta_l / v;
-
-            output_beam.push(Electron {
-                t: new_t,
-                ke: electron.ke,
+        } else if contents.starts_with('}') {
+            chop_character(contents);
+            tokens.push(Token {
+                token_type: TokenType::Ccurly,
+                value: "}".to_string(),
             });
-        }
-        output_beam
-    }
-}
-
-impl Tracking for AccCav {
-    fn track(&self, beam: Beam) -> Beam {
-        let mut output_beam: Beam = vec![];
-        let egain = self.length * self.voltage;
-        for electron in beam {
-            let phase = self.phi + 2.0 * PI * (electron.t * self.freq);
-            output_beam.push(Electron {
-                t: electron.t,
-                ke: electron.ke + egain * phase.cos(),
+        } else if contents.starts_with(':') {
+            chop_character(contents);
+            tokens.push(Token {
+                token_type: TokenType::Colon,
+                value: ":".to_string(),
             });
+        } else {
+            let chr = chop_character(contents);
+            eprintln!("Unknown character: {}", chr);
         }
-        output_beam
     }
+    tokens
 }
-
-struct Accelerator {
-    pub elements: Vec<Box<dyn Tracking>>,
-}
-
-impl Accelerator {
-    fn track(&self, beam: Beam) -> Beam {
-        let mut output_beam = beam;
-        for element in self.elements.iter() {
-            output_beam = element.track(output_beam.clone());
-        }
-        output_beam
-    }
-}
-
-// TODO: Electrons may be better described as a simple array. Look at ndarray.
-#[derive(Copy, Clone)]
-pub struct Electron {
-    t: f64,
-    ke: f64,
-}
-
-type Beam = Vec<Electron>;
 
 fn main() {
-    let design_ke = 1e8;
-    let cav_length = 6.0;
-    let cav_voltage = 20e6;
-    let cav_freq = 3e9;
-    let cav_phi: f64 = -1.56;
-    let cav_egain = cav_voltage * cav_length * cav_phi.cos();
-    let design_gamma = (design_ke + 4.0 * cav_egain) / MASS;
+    let filename = "acc_defn.lotr";
+    let mut contents = read_to_string(filename).expect("Could not read file.");
 
-    let bunch_compressor = Accelerator {
-        elements: vec![
-            // Acceleration
-            Box::new(AccCav::new(cav_length, cav_voltage, cav_freq, cav_phi)),
-            Box::new(Drift::new(0.5, (design_ke + cav_egain) / MASS)),
-            Box::new(AccCav::new(cav_length, cav_voltage, cav_freq, cav_phi)),
-            Box::new(Drift::new(0.5, (design_ke + 2.0 * cav_egain) / MASS)),
-            Box::new(AccCav::new(cav_length, cav_voltage, cav_freq, cav_phi)),
-            Box::new(Drift::new(0.5, (design_ke + 3.0 * cav_egain) / MASS)),
-            Box::new(AccCav::new(cav_length, cav_voltage, cav_freq, cav_phi)),
-            Box::new(Drift::new(0.5, design_gamma)),
-            // Bunch compressor chicane
-            Box::new(Dipole::new(1.0, 2.0, design_gamma)),
-            Box::new(Drift::new(1.0, design_gamma)),
-            Box::new(Dipole::new(1.0, -2.0, design_gamma)),
-            Box::new(Drift::new(1.0, design_gamma)),
-            Box::new(Dipole::new(1.0, -2.0, design_gamma)),
-            Box::new(Drift::new(1.0, design_gamma)),
-            Box::new(Dipole::new(1.0, 2.0, design_gamma)),
-            Box::new(Drift::new(1.0, design_gamma)),
-        ],
-    };
-
-    let beam = vec![
-        Electron {
-            t: -10e-12,
-            ke: design_ke,
-        },
-        Electron {
-            t: 0.0,
-            ke: design_ke,
-        },
-        Electron {
-            t: 10e-12,
-            ke: design_ke,
-        },
-    ];
-
-    println!("---   INPUT  ---");
-    for electron in &beam {
-        println!(
-            "{:0.6} ps :: {:0.3} MeV",
-            electron.t * 1e12,
-            electron.ke * 1e-6
-        );
-    }
-    // let out_beam = drift.track(beam);
-    // let out_beam = bend.track(beam);
-    println!("--- TRACKING ---");
-    let out_beam = bunch_compressor.track(beam);
-    println!("---  OUTPUT  ---");
-
-    for electron in out_beam {
-        println!(
-            "{:0.6} ps :: {:0.3} MeV",
-            electron.t * 1e12,
-            electron.ke * 1e-6
-        );
-    }
+    let tokens = tokenize_file_contents(&mut contents);
+    println!("{:?}", tokens);
 }
