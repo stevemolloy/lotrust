60fbfb847b41f7b4bf0aed9097c08d8f28e8141c
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-04-07 13:31:45 +0200

    Parser understands negative values

--- src/main.rs
+++ src/main.rs
@@ -180,29 +180,29 @@ fn parse_word(input: &mut String) -> Token {
 fn parse_digit(input: &mut String) -> Token {
     let mut already_decimal: bool = false;
     let mut already_exp: bool = false;
-    let mut name: String = chop_character(input).to_string();
+    let mut value: String = chop_character(input).to_string();
     while !input.is_empty() {
         if input.starts_with(|c: char| c.is_ascii_digit()) {
-            name.push(chop_character(input));
+            value.push(chop_character(input));
         } else if input.starts_with('e') {
             if already_exp {
                 panic!("Attempt to add 'e' to a digit twice");
             }
             already_exp = true;
-            name.push(chop_character(input));
+            value.push(chop_character(input));
         } else if input.starts_with('.') {
             if already_decimal {
                 panic!("Attempt to add a second decimal point to a digit");
             }
             already_decimal = true;
-            name.push(chop_character(input));
+            value.push(chop_character(input));
         } else {
             break;
         }
     }
     Token {
         token_type: TokenType::Value,
-        value: name,
+        value: value,
     }
 }
 
@@ -218,7 +218,7 @@ fn tokenize_file_contents(contents: &mut String) -> Vec<Token> {
             continue;
         } else if contents.starts_with(|c: char| c.is_ascii_alphabetic()) {
             tokens.push(parse_word(contents));
-        } else if contents.starts_with(|c: char| c.is_ascii_digit()) {
+        } else if contents.starts_with(|c: char| c.is_ascii_digit() || c=='-') {
             tokens.push(parse_digit(contents));
         } else if contents.starts_with('{') {
             chop_character(contents);
