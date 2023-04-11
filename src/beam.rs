3645f2376ccc3faaf0f808673cb6beb6e0ff06d3
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-04-11 15:42:02 +0200

    Improved tests

--- src/beam.rs
+++ src/beam.rs
@@ -23,20 +23,25 @@ pub fn gamma_2_beta(g: f64) -> f64 {
     (1f64 - (1f64 / g.powi(2))).sqrt()
 }
 
-#[test]
-fn ke_of_restmass_has_gamma_two() {
-    let electron = Electron { t: 0f64, ke: MASS };
-    assert_eq!(electron.gamma(), 2.0);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn zero_ke_has_unity_gamma() {
-    let electron = Electron { t: 0f64, ke: 0f64 };
-    assert_eq!(electron.gamma(), 1.0);
-}
+    #[test]
+    fn ke_of_restmass_has_gamma_two() {
+        let electron = Electron { t: 0f64, ke: MASS };
+        assert_eq!(electron.gamma(), 2.0);
+    }
 
-#[test]
-fn zero_ke_has_zero_beta() {
-    let electron = Electron { t: 0f64, ke: 0f64 };
-    assert_eq!(gamma_2_beta(electron.gamma()), 0.0);
+    #[test]
+    fn zero_ke_has_unity_gamma() {
+        let electron = Electron { t: 0f64, ke: 0f64 };
+        assert_eq!(electron.gamma(), 1.0);
+    }
+
+    #[test]
+    fn zero_ke_has_zero_beta() {
+        let electron = Electron { t: 0f64, ke: 0f64 };
+        assert_eq!(gamma_2_beta(electron.gamma()), 0.0);
+    }
 }
