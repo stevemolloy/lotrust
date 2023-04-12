3c456ab47c90020886b0ccf1f15a9f3c60a701c6
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-04-12 23:07:41 +0200

    Coord change almost done. AccCav is wrong at the moment

--- src/elements.rs
+++ src/elements.rs
@@ -1,4 +1,4 @@
-use crate::beam::{gamma_2_beta, Beam};
+use crate::beam::{gamma_2_beta, Beam, MASS, C};
 use ndarray::{arr2, Array2};
 // use std::f64::consts::PI;
 
@@ -7,8 +7,6 @@ pub trait Tracking {
 }
 
 pub struct Drift {
-    length: f64,
-    gamma0: f64,
     t_matrix: Array2<f64>,
 }
 
@@ -18,8 +16,6 @@ impl Drift {
         let gamma_sq = g.powi(2);
         let r56 = l / (beta_sq * gamma_sq);
         Drift {
-            length: l,
-            gamma0: g,
             t_matrix: arr2(&[[1f64, r56], [0f64, 1f64]]),
         }
     }
@@ -36,50 +32,46 @@ pub type Quad = Drift;
 pub type Sext = Drift;
 
 pub struct Dipole {
-    b_field: f64,
-    theta: f64,
-    gamma0: f64,
+    t_matrix: Array2<f64>,
 }
 
 impl Dipole {
     pub fn new(b: f64, angle: f64, g: f64) -> Dipole {
+        let pc = (g.powi(2) - 1.0).sqrt() * MASS;
+        let rho = pc / (C * b);
+        let omega = 1f64 / rho;
+        let l = rho * angle;
+        let omega_l = omega * l;
+        let beta_sq = gamma_2_beta(g).powi(2);
+        let gamma_sq = g.powi(2);
+        let r56 = l / (beta_sq*gamma_sq) - (omega_l - omega_l.sin()) / (omega*beta_sq);
         Dipole {
-            b_field: b,
-            theta: angle,
-            gamma0: g,
+            t_matrix: arr2(&[[1f64, r56], [0f64, 1f64]]),
         }
     }
 }
 
 impl Tracking for Dipole {
-    // The commented lines in this function calculate the change in the angle
-    // due to the different radius of curvature, but in practise the difference
-    // this makes in the timing is *tiny*
     fn track(&self, beam: &mut Beam) {
-        todo!();
+        *beam = beam.dot(&self.t_matrix.t());
     }
 }
 
 pub struct AccCav {
-    length: f64,
-    voltage: f64,
-    freq: f64,
-    phi: f64,
+    t_matrix: Array2<f64>,
 }
 
 impl AccCav {
     pub fn new(l: f64, v: f64, freq: f64, phi: f64) -> AccCav {
+        let blah = l + v + freq + phi;
         AccCav {
-            length: l,
-            voltage: v,
-            freq,
-            phi,
+            t_matrix: arr2(&[[1f64, blah/blah], [0f64, 1f64]]),
         }
     }
 }
 
 impl Tracking for AccCav {
     fn track(&self, beam: &mut Beam) {
-        todo!();
+        *beam = beam.dot(&self.t_matrix.t());
     }
 }
